06275ffad6ae33addfc6b9b35ebc3141515d2916
TheHollyCow <impostae3@gmail.com>
2026-05-29 13:09:36 -0600

    Add Discord RPC presence to electron-main.js

--- electron-main.js
+++ electron-main.js
@@ -1,5 +1,32 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
+const DiscordRPC = require("discord-rpc");
+
+const DISCORD_CLIENT_ID = "1509996292796059718";
+DiscordRPC.register(DISCORD_CLIENT_ID);
+
+const rpc = new DiscordRPC.Client({ transport: "ipc" });
+const startedAt = Date.now();
+
+function setDiscordPresence() {
+  rpc.setActivity({
+    details: "Climbing the antes",
+    state: "Playing BLACKJACK: ANTEFALL",
+    startTimestamp: startedAt,
+    largeImageKey: "logo",
+    largeImageText: "BLACKJACK: ANTEFALL",
+    buttons: [
+      {
+        label: "Download",
+        url: "https://github.com/Flopper1-1/BLACKJACK-ANTEFALL"
+      }
+    ]
+  });
+}
+
+rpc.on("ready", setDiscordPresence);
+
+rpc.login({ clientId: DISCORD_CLIENT_ID }).catch(() => {});
 
 function createWindow() {
   const mainWindow = new BrowserWindow({
